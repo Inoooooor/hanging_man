709ec3b8c79b76747e1e859ec6da33b2e85d2f0b
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2022-12-03 19:42:24 +0300

    Added_game_logic

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
 const testButton = document.getElementById("test_button");
 const hangmanPic = document.getElementById("hangman_pic");
+const choiceLetterArr = document.getElementsByClassName("letter");
+const missingLettersArr = document.getElementsByClassName("missing_letter");
 let hangmanProgress = 0;
+let chosenLetterBuffer = 0;
+let lettersMatch = 0;
+
+console.log(choiceLetterArr.length);
+
+let gameOver = () => {
+    if (hangmanProgress == 5) window.stop();
+}
 
 function hangmanDrawing () {
-    if (hangmanProgress == 5) hangmanProgress = 0;
     hangmanProgress++;
     if (hangmanProgress == 1) hangmanPic.src = "src/hangman_pics/rope.png";
-     else if (hangmanProgress == 2) hangmanPic.src = "src/hangman_pics/head.png";
-     else if (hangmanProgress == 3) hangmanPic.src = "src/hangman_pics/body.png";
-     else if (hangmanProgress == 4) hangmanPic.src = "src/hangman_pics/hands.png";
-     else if (hangmanProgress == 5) hangmanPic.src = "src/hangman_pics/legs.png";
+    else if (hangmanProgress == 2) hangmanPic.src = "src/hangman_pics/head.png";
+    else if (hangmanProgress == 3) hangmanPic.src = "src/hangman_pics/body.png";
+    else if (hangmanProgress == 4) hangmanPic.src = "src/hangman_pics/hands.png";
+    else if (hangmanProgress == 5) hangmanPic.src = "src/hangman_pics/legs.png";
     console.log(hangmanProgress);
 }
 
-testButton.addEventListener("click", hangmanDrawing);
+function letterChecking() {
+    for (let i = 1; i < missingLettersArr.length - 1; i++) {
+        if (chosenLetterBuffer == missingLettersArr[i].textContent) {
+            missingLettersArr[i].style.color = "black";
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+// testButton.addEventListener("click", hangmanDrawing);
+
+// console.log(choiceLetterArr[20].value);
+
+// Added clicking on every letter 
+
+for (let i = 0; i < choiceLetterArr.length; i++) choiceLetterArr[i].addEventListener("click", () => {
+    chosenLetterBuffer = choiceLetterArr[i].value;
+    if (letterChecking()) {
+        choiceLetterArr[i].classList.add("right_letter");
+        choiceLetterArr[i].setAttribute("disabled", "disabled");
+    } else {
+        choiceLetterArr[i].classList.add("wrong_letter");
+        choiceLetterArr[i].setAttribute("disabled", "disabled");
+        hangmanDrawing();
+        gameOver();
+    }
+    console.log(chosenLetterBuffer);
+});
+
+// Hiding missing letters 
+
+for (let i = 1; i < missingLettersArr.length - 1; i++) {
+    missingLettersArr[i].style.color = "transparent";
+}
+
+
+// Letter checkiing except hint letters 
